3b615e2f8df12f336fb67307ffc1853490ff85c4
lycioandreatto <lycioandreatto13@gmail.com>
2026-09-11 16:22:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ st.markdown(
         gap: 5px;
         flex-wrap: nowrap;
 
-        background: #FAF7F5;
+        background: var(--orange);
         border: 1px solid #EEE7E3;
         border-radius: 14px;
 
