8fba84abbf48cf52c7b11414298c45d1c49887be
lycioandreatto <lycioandreatto13@gmail.com>
2026-09-11 16:10:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1288,60 +1288,53 @@ else:
 # MENU SUPERIOR
 # ============================================================
 
-st.markdown(
-    """
-    <div class="topbar">
-        <div class="topbar-inner">
+topbar_col1, topbar_col2 = st.columns(
+    [1.1, 5],
+    vertical_alignment="center"
+)
 
-            <div class="topbar-brand">
+with topbar_col1:
 
-                <div class="topbar-logo">
-                    A
+    st.markdown(
+        """
+        <div class="topbar-brand">
+
+            <div class="topbar-logo">
+                A
+            </div>
+
+            <div>
+                <div class="topbar-brand-name">
+                    Aura
                 </div>
 
-                <div>
-                    <div class="topbar-brand-name">
-                        Aura
-                    </div>
-
-                    <div class="topbar-brand-subtitle">
-                        Beauty Studio
-                    </div>
+                <div class="topbar-brand-subtitle">
+                    Beauty Studio
                 </div>
-
             </div>
 
         </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+        """,
+        unsafe_allow_html=True
+    )
 
-st.markdown(
-    '<div class="topbar-nav">',
-    unsafe_allow_html=True
-)
+with topbar_col2:
 
-pagina = st.radio(
-    "Navegação",
-    [
-        "⌂  Visão geral",
-        "▣  Agenda",
-        "＋  Novo agendamento",
-        "♡  Clientes",
-        "✦  Serviços",
-        "R$  Financeiro",
-        "↗  Gastos",
-        "♧  Fidelização"
-    ],
-    horizontal=True,
-    label_visibility="collapsed"
-)
-
-st.markdown(
-    '</div>',
-    unsafe_allow_html=True
-)
+    pagina = st.radio(
+        "Navegação",
+        [
+            "⌂  Visão geral",
+            "▣  Agenda",
+            "＋  Novo agendamento",
+            "♡  Clientes",
+            "✦  Serviços",
+            "R$  Financeiro",
+            "↗  Gastos",
+            "♧  Fidelização"
+        ],
+        horizontal=True,
+        label_visibility="collapsed"
+    )
 
 pagina = (
     pagina
